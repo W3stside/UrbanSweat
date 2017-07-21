109cc3f699948046fa9397c4fd386bbf30ddc91c
David Sato <david.sato64@gmail.com>
2017-07-20 22:49:52 -0700

    edit

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -43,16 +43,31 @@ module.exports = {
                 }
             },
             {
+                test: /\.jsx$/,
+                loader: "babel-loader", // Do not use "use" here
+            },    
+            {
                 test: /\.(png|jpg)$/,
-                loader: 'url-loader?limit=8192'
+                use: [
+                    {
+                        loader: 'url-loader?limit=8192'
+                    }
+                ]
             },
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader'
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
             },
             {
                 test: /\.scss$/,
-                loader: 'style-loader!css-loader!sass-loader'
+                use: [
+                    {
+                        loader: 'style-loader!css-loader!sass-loader'
+                    }
+                ]
             }
         ]
     }
