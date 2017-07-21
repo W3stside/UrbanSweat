46e05e18b6bacd7ff291d92d16c52254329eca57
David Sato <david.sato64@gmail.com>
2017-07-20 18:17:06 -0700

    testing build...

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   entry: './src/index.js',
   output: {
-    path: './dist',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: ''
   },
