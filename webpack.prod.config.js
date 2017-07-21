9d37d1caa55162763b8da5f85bce883c544809a7
David Sato <david.sato64@gmail.com>
2017-07-20 17:54:30 -0700

    testing build...

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: `${__dirname}/dist`,
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: ''
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
