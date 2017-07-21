f1842d387cea7fcc585a26f556f289c6dc0e4cb5
David Sato <david.sato64@gmail.com>
2017-07-20 21:17:19 -0700

    edit

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -27,24 +27,7 @@ module.exports = {
   ],
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        loaders: [ 'babel' ],
-        exclude: /node_modules/,
-        include: __dirname
-      },
-      {
-        test: /\.(png|jpg)$/,
-        loader: 'url-loader?limit=8192'
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-      {
-        test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader'
-      }
+      
     ]
   }
 }
